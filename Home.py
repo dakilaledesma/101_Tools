6d8fd9f5f84110eaa2b2f69d074f8978363d4e53
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2023-12-05 20:05:58 -0500

    rename link to grade visualizer

--- Home.py
+++ Home.py
@@ -5,4 +5,6 @@ st.set_page_config(layout="wide")
 
 st.title("101L Tools")
 st.write("Welcome. Please select a tool from the sidebar or below:")
-st.markdown("[Go to Page 1](/Grade_Visualizer)")
+
+st.subheader("Available Tools")
+st.markdown("[Grade Visualizer](/Grade_Visualizer)")
